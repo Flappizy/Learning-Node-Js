933f171d500017103e496506ae10b601ddb4e502
Flappizy <75231665+Flappizy@users.noreply.github.com>
2023-02-10 09:53:15 +0100

    revealing-constructor-pattern

---
+++ js/DesignPatterns/Creational/RevealingConstructor/immutableBuffer.js
@@ -0,0 +1,22 @@
+const MODIFIER_NAMES = ['swap', 'write', 'fill'];
+export class ImmutableBuffer {
+    constructor(size, executor) {
+        const modifiers = {};
+        const buffer = Buffer.alloc(size);
+        
+        for (const prop in buffer) {
+            if (typeof buffer[prop] !== 'function') {
+                continue;
+            }
+
+            if (MODIFIER_NAMES.some(m => prop.startsWith(m))) {
+                modifiers[prop] = buffer[prop].bind(buffer);
+            }
+            else{
+                this[prop] = buffer[prop].bind(buffer);
+            }
+        }
+
+        executor(modifiers);
+    }
+}
