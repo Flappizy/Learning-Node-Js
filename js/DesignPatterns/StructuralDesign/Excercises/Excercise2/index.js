16ef932f077653ce478373fa3762db7630e1499b
Flappizy <75231665+Flappizy@users.noreply.github.com>
2023-02-23 19:28:23 +0100

    chapter-8-excercise-2

---
+++ js/DesignPatterns/StructuralDesign/Excercises/Excercise2/index.js
@@ -0,0 +1,6 @@
+import { consoleProxy } from "./proxy-console.js";
+
+consoleProxy.log("Hello, my name is Ibukun Babatunde");
+consoleProxy.error("Hello, this is an ERROR from Ibukun Babatunde");
+consoleProxy.debug("Hello, this is a debug message from Ibukun Babatunde");
+consoleProxy.info("Hello, this is an INFO message from Ibukun Babatunde");
---
+++ js/DesignPatterns/StructuralDesign/Excercises/Excercise2/proxy-console.js
@@ -0,0 +1,14 @@
+const logMethods = ['log', 'debug', 'info', 'error'];
+
+const handler = {
+    get: (target, prop) => {
+        if (logMethods.includes(prop)) {
+            return function(...args) {
+                target[prop](`${new Date()}: ${args}`);
+            }               
+        }
+        return target[prop];
+    }
+}
+
+export const consoleProxy = new Proxy(console, handler);
